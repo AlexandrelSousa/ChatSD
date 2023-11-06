07e1f6a8cda4ae345d6df424426309b8d0509e5b
Alexandre lopes de sousa <alexandrelsousa@outlook.com>
2023-11-06 15:49:57 -0300

    main
    Inicializando o projeto.

---
+++ src/test/java/chatcafe/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package chatcafe.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
