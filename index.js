96c4f5e971be26ea1623b81739aa27a98b076838
Alexandre lopes de sousa <alexandrelsousa@outlook.com>
2023-11-28 01:03:19 -0300

    main
    finalizando a comunicação entre o back e o front através do socket.

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}
 
 const PORT = 3003
 
+io.on('connection', socket=> {
+    console.log("usuario conectado.", socket.id);
+
+    socket.on('disconnect', reason => {
+        console.log("Usuário desconectado!", socket.id)
+    })
+
+    socket.on('setUserName', username => {
+        socket.data.username = username
+        console.log(socket.data.username)
+    })
+
+    socket.on('message', text=>{
+        io.emit('receive_Message', {
+            text,
+            authorID: socket.id,
+            author: socket.data.username
+        })
+    })
+})
+
 server.listen(PORT, () => console.log("Server running..."));
